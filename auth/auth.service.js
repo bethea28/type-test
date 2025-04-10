f96e1e550cebfc5e338e43d68f0e447a3730869f
bethea28 <bryanbethea28@gmail.com>
2025-04-09 20:13:39 -0400

    basic auth

--- auth/auth.service.js
+++ auth/auth.service.js
@@ -1,5 +1,7 @@
 "use strict";
 // auth/auth.service.js
+const { Sequelize } = require("sequelize"); // Import the Sequelize constructor
+
 const User = require("../user/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -8,25 +10,38 @@ const authService = {
     try {
       // Check if user with the same username or email already exists
       const existingUser = await User.findOne({
-        $or: [{ username }, { email }],
+        where: {
+          [Sequelize.Op.or]: [{ username }, { email }], // Use Sequelize.Op directly
+        },
       });
       if (existingUser) {
         throw new Error("Username or email already exists");
       }
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const newUser = new User({ username, email, password: hashedPassword });
-      return await newUser.save();
+      // const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = await User.create({
+        username,
+        email,
+        password,
+        // password: hashedPassword,
+      });
+      return newUser; // Sequelize's create method returns the created instance
     } catch (error) {
       console.error("Error in authService.registerUser:", error);
       throw error;
     }
   },
   async loginUser(email, password) {
+    console.log("LOGGIN IN NOW", email, password);
     try {
-      const user = await User.findOne({ email }).select("+password"); // Select the password field
-      if (!user || !(await bcrypt.compare(password, user.password))) {
+      const user = await User.findOne({ where: { email } });
+      console.log("USER WAS GOOD", user);
+      // const user = await User.findOne({ email }).select("+password"); // Select the password field
+      if (!user) {
         return null; // Invalid credentials
       }
+      // if (!user || !(await bcrypt.compare(password, user.password))) {
+      //   return null; // Invalid credentials
+      // }
       // Generate a JWT token
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
         expiresIn: "1h", // Example expiration
