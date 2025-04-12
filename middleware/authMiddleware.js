91f9e45e4403f339757e62faa90b785acb9edb97
bethea28 <bryanbethea28@gmail.com>
2025-04-12 14:13:59 -0400

    one sided story ready working

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,15 +4,15 @@ const User = require("../user/user.model"); // Adjust the path to your User mode
 
 const authenticateToken = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  const token = authHeader;
-  //   const token = authHeader a&& authHeader.split(" ")[1]; // Extract Bearer token
-  console.log("here is my authentoken middle", authHeader);
+  // const token = authHeader;
+  const token = authHeader && authHeader.split(" ")[1]; // Extract Bearer token
+  console.log("here is my authentoken middle BRYANB ", token);
   if (!token) {
     return res.status(401).json({ error: "Unauthorized: No token provided." });
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const userId = decoded.id; // Assuming your JWT payload has 'id' as the user ID
 
     console.log("USER IS NOW FINDING", userId);
