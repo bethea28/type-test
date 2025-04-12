cb4707ef01278a9a27f7103619dd34d0f9884561
bethea28 <bryanbethea28@gmail.com>
2025-04-12 16:52:57 -0400

    correct env usage

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,15 +4,16 @@ const User = require("../user/user.model"); // Adjust the path to your User mode
 
 const authenticateToken = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  // const token = authHeader;
+  // const token = authHeader; // use for postman
   const token = authHeader && authHeader.split(" ")[1]; // Extract Bearer token
-  console.log("here is my authentoken middle BRYANB ", token);
+  console.log("here is my authentoken middle", authHeader);
   if (!token) {
     return res.status(401).json({ error: "Unauthorized: No token provided." });
   }
-
+  console.log("ENV SECRETS", process.env.ACCESS_TOKEN_SECRET);
   try {
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const decoded = jwt.verify(token, "octheheroaccess"); // use for postman only
+    // const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const userId = decoded.id; // Assuming your JWT payload has 'id' as the user ID
 
     console.log("USER IS NOW FINDING", userId);
